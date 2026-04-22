c306019972af4284777a85b00eee9333f677d9b0
AyatT12 <105029664+AyatT12@users.noreply.github.com>
2026-04-22 22:12:47 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+import jsonServer from 'json-server'
+
+const server = jsonServer.create()
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  next()
+})
+
+server.use(middlewares)
+server.use(router)
+
+server.listen(3001, () => {
+  console.log('JSON Server is running on port 3001')
+})
